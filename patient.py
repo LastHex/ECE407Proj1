aaa0f489477d3809553a8a8680de9c7255e970e6
Sam Miller <smillerc@live.com>
2016-01-27 11:08:44 -0500

    Added the feature to load the ecg file all at once and fixed the plot routine. Also fixed the bug where load_ecg_file called load_ecg_header if it hadnt before, which required load_ecg_file to be called twice.

--- patient.py
+++ patient.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 class Patient:
+
     """
     Class that handles the patient info and ecg files
     """
@@ -53,22 +54,22 @@ class Patient:
                 print("Reading filename (header only): " + filename)
 
                 self.magic_number = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a8'), count=1)[0]
+                    self.ecg_file, dtype=np.dtype('a8'), count=1)[0]
                 self.checksum = np.fromfile(self.ecg_file, dtype=np.uint16, count=1)[0]
                 self.var_length_block_size = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
+                    self.ecg_file, dtype=np.int32, count=1)[0]
                 self.sample_size_ecg = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
+                    self.ecg_file, dtype=np.int32, count=1)[0]
                 self.offset_var_length_block = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
+                    self.ecg_file, dtype=np.int32, count=1)[0]
                 self.offset_ecg_block = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
+                    self.ecg_file, dtype=np.int32, count=1)[0]
                 self.file_version = np.fromfile(
-                        self.ecg_file, dtype=np.int16, count=1)[0]
+                    self.ecg_file, dtype=np.int16, count=1)[0]
                 self.first_name = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
+                    self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
                 self.last_name = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
+                    self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
                 self.ID = np.fromfile(self.ecg_file, dtype=np.dtype('a20'), count=1)[0]
                 self.sex = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
                 self.race = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
@@ -82,15 +83,15 @@ class Patient:
                 self.resolution = np.fromfile(self.ecg_file, dtype=np.int16, count=12)
                 self.pacemaker = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
                 self.recorder = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
+                    self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
                 self.sampling_rate = np.fromfile(
-                        self.ecg_file, dtype=np.int16, count=1)[0]
+                    self.ecg_file, dtype=np.int16, count=1)[0]
                 self.proprietary = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
+                    self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
                 self.copyright = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
+                    self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
                 self.reserved = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a88'), count=1)[0]
+                    self.ecg_file, dtype=np.dtype('a88'), count=1)[0]
 
                 # read variable length block
                 if self.var_length_block_size > 0:
@@ -110,90 +111,88 @@ class Patient:
         :return:
         """
 
-        if not self.ecg_file:
-            # Open and read the header
-            self.load_ecg_header(filename)
-        else:
-            try:
-                self.ecg_file = open(filename, 'rb')
-                print("Reading filename (header and data): " + filename)
+        try:
+            self.ecg_file = open(filename, 'rb')
+            print("Reading filename (header and data): " + filename)
 
-                self.magic_number = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a8'), count=1)[0]
-                self.checksum = np.fromfile(self.ecg_file, dtype=np.uint16, count=1)[0]
-                self.var_length_block_size = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
-                self.sample_size_ecg = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
-                self.offset_var_length_block = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
-                self.offset_ecg_block = np.fromfile(
-                        self.ecg_file, dtype=np.int32, count=1)[0]
-                self.file_version = np.fromfile(
-                        self.ecg_file, dtype=np.int16, count=1)[0]
-                self.first_name = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
-                self.last_name = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
-                self.ID = np.fromfile(self.ecg_file, dtype=np.dtype('a20'), count=1)[0]
-                self.sex = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
-                self.race = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
-                self.birth_date = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
-                self.record_date = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
-                self.file_date = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
-                self.start_time = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
-                self.n_leads = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
-                self.lead_spec = np.fromfile(self.ecg_file, dtype=np.int16, count=12)
-                self.lead_quality = np.fromfile(self.ecg_file, dtype=np.int16, count=12)
-                self.resolution = np.fromfile(self.ecg_file, dtype=np.int16, count=12)
-                self.pacemaker = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
-                self.recorder = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
-                self.sampling_rate = np.fromfile(
-                        self.ecg_file, dtype=np.int16, count=1)[0]
-                self.proprietary = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
-                self.copyright = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
-                self.reserved = np.fromfile(
-                        self.ecg_file, dtype=np.dtype('a88'), count=1)[0]
+            self.magic_number = np.fromfile(
+                self.ecg_file, dtype=np.dtype('a8'), count=1)[0]
+            self.checksum = np.fromfile(self.ecg_file, dtype=np.uint16, count=1)[0]
+            self.var_length_block_size = np.fromfile(
+                self.ecg_file, dtype=np.int32, count=1)[0]
+            self.sample_size_ecg = np.fromfile(
+                self.ecg_file, dtype=np.int32, count=1)[0]
+            self.offset_var_length_block = np.fromfile(
+                self.ecg_file, dtype=np.int32, count=1)[0]
+            self.offset_ecg_block = np.fromfile(
+                self.ecg_file, dtype=np.int32, count=1)[0]
+            self.file_version = np.fromfile(
+                self.ecg_file, dtype=np.int16, count=1)[0]
+            self.first_name = np.fromfile(
+                self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
+            self.last_name = np.fromfile(
+                self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
+            self.ID = np.fromfile(self.ecg_file, dtype=np.dtype('a20'), count=1)[0]
+            self.sex = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
+            self.race = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
+            self.birth_date = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
+            self.record_date = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
+            self.file_date = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
+            self.start_time = np.fromfile(self.ecg_file, dtype=np.int16, count=3)
+            self.n_leads = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
+            self.lead_spec = np.fromfile(self.ecg_file, dtype=np.int16, count=12)
+            self.lead_quality = np.fromfile(self.ecg_file, dtype=np.int16, count=12)
+            self.resolution = np.fromfile(self.ecg_file, dtype=np.int16, count=12)
+            self.pacemaker = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
+            self.recorder = np.fromfile(
+                self.ecg_file, dtype=np.dtype('a40'), count=1)[0]
+            self.sampling_rate = np.fromfile(
+                self.ecg_file, dtype=np.int16, count=1)[0]
+            self.proprietary = np.fromfile(
+                self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
+            self.copyright = np.fromfile(
+                self.ecg_file, dtype=np.dtype('a80'), count=1)[0]
+            self.reserved = np.fromfile(
+                self.ecg_file, dtype=np.dtype('a88'), count=1)[0]
 
-                # read variable length block
-                if self.var_length_block_size > 0:
-                    self.dt = np.dtype((str, self.var_length_block_size))
-                    self.var_block = np.fromfile(self.ecg_file, dtype=self.dt, count=1)[0]
+            # read variable length block
+            if self.var_length_block_size > 0:
+                self.dt = np.dtype((str, self.var_length_block_size))
+                self.var_block = np.fromfile(self.ecg_file, dtype=self.dt, count=1)[0]
 
-                # ECG data
-                self.samples_per_lead = self.sample_size_ecg / self.n_leads
-                self.ecg_data = np.zeros((self.n_leads, self.samples_per_lead))
+            # ECG data
+            self.samples_per_lead = self.sample_size_ecg / self.n_leads
 
-                # Loop through data (not efficient at all...)
-                # TODO: Fix this to read all in one chunk and split once in memory
-                for i in range(int(self.samples_per_lead)):
-                    for j in range(self.n_leads):
-                        self.ecg_data[j][i] = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
+            ecg_dtype = np.dtype([('samples', np.int16, self.n_leads)])
+            self.ecg_data = np.fromfile(
+                self.ecg_file, dtype=ecg_dtype, count=int(self.samples_per_lead))
+            # for i in range(int(self.samples_per_lead)):
+            #    for j in range(self.n_leads):
+            #        self.ecg_data[j][i] = np.fromfile(self.ecg_file, dtype=np.int16, count=1)[0]
 
-            except IOError:
-                print("File cannot be opened:", filename)
+        except IOError:
+            print("File cannot be opened:", filename)
 
-    def plot_leads(self, leads):
+    def plot_leads(self, leads=None):
         """
-        Plot the ecg leads (0 ordered!). Defaults to lead 1 if none given
-        :param self:
+        Plot the ecg leads (0 ordered!). Defaults to plot all of the leads
         :param leads: nothing (default to 1), integer of lead number, or list of ints
         """
-
         if leads is None:
-            print("No lead number or list provided, defaulting to lead 1...")
-            leads = 1
-
-        if isinstance(leads, list):
-            pass
-
-        elif isinstance(leads, int):
-            # Make it a list of a single for easier logic below
+            print("No lead number or list provided, defaulting to plot all...")
+            leads = self.lead_spec
+        elif isinstance(leads, list):
             _lead = leads
-            leads = [_lead]
+            leads = np.zeros_like(self.lead_spec)
+            for l in _lead:
+                leads[l] = 1
+        elif isinstance(leads, int):
+            _lead = leads
+            leads = np.zeros_like(self.lead_spec)
+            leads[_lead] = 1
+        else:
+            print('Error: plot_leads() argument must be int, a list of ints, or empty for all leads')
+            return
 
         # Try to import matplotlib
         try:
@@ -203,23 +202,27 @@ class Patient:
             return
 
         # Check to make sure ecg data is loaded
-        if not self.ecg_data.any():
+        if self.ecg_data.size == 0:
             print("ECG data not loaded yet...")
             return
 
         # Loop through given leads to plot
-        for lead in leads:
-            # Resolution[0] = 10000nv = 0.01mv
-            y = self.ecg_data[lead - 1][:] * (self.resolution[lead - 1] / 1000000.0)
-            x = np.linspace(0, self.samples_per_lead / self.sampling_rate,
-                            num=self.samples_per_lead)
-            plt.plot(x,y)
-            plt.title('ECG')
-            plt.xlabel('s')
-            plt.ylabel('mV')
-            plt.xlim(0, 5)
-            plt.ylim(-1, 1)
-
+        for index, lead in enumerate(leads):
+            if lead != 1:
+                pass
+            else:
+                print("Plotting lead: " + str(index))
+                # Resolution[0] = 10000nv = 0.01mv
+                y = self.ecg_data['samples'][:, index] * (self.resolution[index] / 1000000.0)
+                x = np.linspace(0, self.samples_per_lead / self.sampling_rate,
+                                num=self.samples_per_lead)
+                plt.plot(x, y, label='lead ' + str(index))
+                plt.title('ECG')
+                plt.xlabel('s')
+                plt.ylabel('mV')
+                plt.xlim(0, 5)
+                plt.ylim(-1, 1)
+                plt.legend()
         plt.show()
 
         return
